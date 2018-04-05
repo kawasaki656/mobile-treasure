db30706ef62459ccc1c97aa1e0fd4883a5d483b2
kawasaki656 <kawasaki646@gmail.com>
2018-04-05 08:52:00 +0300

    add events

---
+++ src/pages/event/event.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import {IonicPage, NavController, NavParams, ViewController} from 'ionic-angular';
+
+/**
+ * Generated class for the EventPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-event',
+  templateUrl: 'event.html',
+})
+export class EventPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public viewCtrl: ViewController) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad EventPage');
+  }
+
+  dismiss() {
+    this.viewCtrl.dismiss();
+  }
+
+}
